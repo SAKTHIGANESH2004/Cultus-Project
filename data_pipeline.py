ebb86c11ef15e91fc7ce775084dfaebbea8db3eb
SAKTHIGANESH2004 <144411918+SAKTHIGANESH2004@users.noreply.github.com>
2025-11-27 12:46:13 +0545

    Delete data_pipeline.py

--- data_pipeline.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-def create_dataset(series, window=30):
-    X, y = [], []
-    for i in range(len(series)-window):
-        X.append(series[i:i+window])
-        y.append(series[i+window])
-    return np.array(X), np.array(y)
